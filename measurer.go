a4d218ae9f2e6454f951a560a362299a4d47662f
Simone Basso <bassosimone@gmail.com>
2021-08-04 10:05:14 +0200

    refactor(measurer.go): introduce measurer to perform measurement
    Let's avoid the pattern where an object writes into itself.
    
    Also, to see each request we need a measurer anyway.

--- measurer.go
+++ measurer.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"sync"
 	"time"
 )
@@ -28,6 +29,9 @@ type httpMeasurement struct {
 type httpResponse struct {
 	// Code is the status code.
 	Code int
+
+	// BodySize is the size of the response body.
+	BodySize int
 
 	// Idx is the index of this response in the redirect chain. The
 	// last response in the chain has index zero.
@@ -66,27 +70,48 @@ func measure(URL *URLEntry) *httpMeasurement {
 // measureWithContext is like measure but with a context.
 func measureWithContext(ctx context.Context, URL *URLEntry) *httpMeasurement {
 	m := &httpMeasurement{OrigURL: URL.URL, Filename: URL.Filename}
-	m.do(ctx, URL.URL)
+	newMeasurer(m).do(ctx, URL.URL)
 	return m
 }
 
+// measurer performs the real measurement. All fields are
+// mandatory. Use newMeasurer to construct a measurer.
+type measurer struct {
+	// clnt is the underlying client.
+	clnt *http.Client
+
+	// m is the measurement we're filling.
+	m *httpMeasurement
+}
+
+// newMeasurer creates a new instance of measurer.
+func newMeasurer(m *httpMeasurement) *measurer {
+	return &measurer{
+		clnt: &http.Client{
+			Transport: &http.Transport{},
+			Jar:       &cookiejar.Jar{},
+		},
+		m: m,
+	}
+}
+
 // do performs the measurement and saves the results into m.
-func (m *httpMeasurement) do(ctx context.Context, URL string) {
+func (m *measurer) do(ctx context.Context, URL string) {
 	req, err := http.NewRequestWithContext(ctx, "GET", URL, nil)
 	if err != nil {
 		f := err.Error()
-		m.Failure = &f
+		m.m.Failure = &f
 		return
 	}
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		f := err.Error()
-		m.Failure = &f
+		m.m.Failure = &f
 		return
 	}
 	defer resp.Body.Close()
 	for idx := 0; resp != nil; idx++ {
-		m.Responses = append(m.Responses, httpResponse{
+		m.m.Responses = append(m.m.Responses, httpResponse{
 			Code: resp.StatusCode,
 			Idx:  idx,
 			Request: httpRequest{
